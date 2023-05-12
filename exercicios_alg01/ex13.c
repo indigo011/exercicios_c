fd24d236da39ba5313898897c9065ebefefc1391
Nicolas <nicolasdaniel2106@gmail.com>
2023-05-12 08:17:59 -0400

    refazendo ex13

--- exercicios_alg01/ex13.c
+++ exercicios_alg01/ex13.c
@@ -1,38 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    int i, j, fatorial, n;
-    float s = 1;
+int main(){
+    int n, i;
+    float s, fatorial;
 
-    printf("Digite um valor inteiro maior que zero: ");
+    s = fatorial = i = 1; //inicializando variáveis
+
+    printf("Digite um numero inteiro maior que zero: ");
     scanf("%d", &n);
 
-    //verificação do n != 0  
-    if (n <= 0) 
-        printf("\nValor invalido!\n");
-    
-    else {
-        //cálculo de cada termo do S
-        for (i = 1; i <= n; i++) {
-            if (i == 1)
-                s++;
-            
-            else {
-                fatorial = i;
-
-                //cálculo do fatorial
-                for (j = i - 1; j >= 1; j--) {
-                    fatorial *= j;
-                }
-
-                s += 1/fatorial;
-            }
-        }
-
-        printf("\n\nS = %.2f\n", s);
+    //calculando o fatorial
+    while (i <= n) {
+        fatorial *= i;
+        s += 1/fatorial; //calculando s
+        i++;
     }
 
-    system("pause");
+    if (n < 1) //restrições
+        printf("\nValor inválido.");
+
+    printf("\nS = %.2f", s);
+
     return 0;
 }
