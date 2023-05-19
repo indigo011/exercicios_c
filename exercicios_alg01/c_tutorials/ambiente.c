4e430c3048c46cd53b0278c415015e8c60193c10
Nicolas <nicolasdaniel2106@gmail.com>
2023-05-19 12:48:02 -0400

    .

--- exercicios_alg01/c_tutorials/ambiente.c
+++ exercicios_alg01/c_tutorials/ambiente.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
-#include <string.h>
-
+#include <locale.h>
+//teste de vetores
 int main() {
-    char nome[25];
+    int i;
+    float notas[4], media;
 
-    printf("Type your name: ");
-    fgets(nome, 25, stdin);
+    setlocale(0, "Portuguese");
 
-    printf("\nHello, %s! How are you?", nome);
+    //calculando a media das notas
+    for (i = 0; i < 4; i++) {
+        printf("Entre com a %dª nota: ", i+1);
+        scanf("%f", &notas[i]);
+
+        media += notas[i];
+    }
+
+    media /= (i+1);
+
+    printf("\n\nA média das notas é %.2f.", media);
+
+    if (media < 5)
+        printf("\nVocê foi reprovado.");
+    else
+        printf("Você foi aprovado.");
+
     return 0;
 }
